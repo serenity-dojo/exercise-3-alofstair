aec9612908094dd2024101cd9650fd1621ed85af
agoodall <agoodall@workforcesoftware.com>
2023-05-12 15:47:38 +0100

    dog test2

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -7,7 +7,7 @@ public class WhenCreatingObjects {
 
     @Test
     public void creating_a_dog() {
-        Dog fido = new Dog("Fido","Bone", 5);
+        Dog fido = new Dog("Fido", "Bone", 5);
         Assert.assertEquals(fido.getName(), "Fido");
         Assert.assertEquals(fido.getFavoriteToy(), "Bone");
         Assert.assertEquals(fido.getAge(), 5);
